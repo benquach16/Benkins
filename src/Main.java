555251b909e15208b1b761af6092d978adad3208
Ben Quach <benquach16@yahoo.com>
2016-05-18 16:48:40 -0700

    move everything into the interface class

--- src/Main.java
+++ src/Main.java
@@ -7,27 +7,10 @@ class IntegrationApp
 {
 	public static void main(String[] args)
 		{
-			Interface d;
-			d = new Interface();
-			try{
-				String[] cmd = new String[]{"/bin/sh", "/home/ben/IntegrationApp/src/bash/vm.sh"};
+			Interface script;
+			script = new Interface();
+			script.run();
 
-				Process pr = Runtime.getRuntime().exec(cmd);
-				pr.waitFor();
-				BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-
-				String line = "";
-
-				while ((line=buf.readLine())!=null) {
-
-					System.out.println(line);
-
-				}
-			System.out.print("sdf");
-			}
-			catch(Exception e)
-			{
-			}			
 		}
 
 };
