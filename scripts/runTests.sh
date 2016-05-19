70f0f52625ee67e3e1b5da179519bc6014eda9da
dylanjay <ddelo004@ucr.edu>
2016-05-18 18:34:41 -0700

    git clone test

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -14,7 +14,18 @@ echo "whoops" >&2
 
 # This script would do something along the lines of pull a repo,
 # run tests, and put the results in /vagrant_data
+username="dylanjay"
+reponame="cs183proj"
+repo="git://github.com/$username/$reponame.git"
+branch="master"
+path="/home/vagrant/Desktop/projTest/test"
+git clone -b $branch $repo $path -q
+forPath="/home/vagrant/Desktop/projTest/test/*.sh"
 
+for f in $forPath
+do
+    sh $f >>/home/vagrant/shared/out.txt 2>&1
+done
 
 
 # poewroff to avoid having to run "vagrant halt" after every
